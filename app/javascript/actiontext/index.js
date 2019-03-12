0d8785a1e5145517c7c694e8cb08e379ddb28534
Hunt Burdick <hunt@edealerdirect.com>
2019-03-12 00:44:13 -0600

    JS: Disable attachments for now.

--- app/javascript/actiontext/index.js
+++ app/javascript/actiontext/index.js
@@ -1,11 +1,11 @@
-import * as Trix from "trix"
-import { AttachmentUpload } from "./attachment_upload"
+// import * as Trix from "trix"
+// import { AttachmentUpload } from "./attachment_upload"
 
-addEventListener("trix-attachment-add", function(event) {
-  const { attachment, target } = event
+// addEventListener("trix-attachment-add", function(event) {
+//   const { attachment, target } = event
 
-  if (attachment.file) {
-    const upload = new AttachmentUpload(attachment, target)
-    upload.start()
-  }
-})
+//   if (attachment.file) {
+//     const upload = new AttachmentUpload(attachment, target)
+//     upload.start()
+//   }
+// })
