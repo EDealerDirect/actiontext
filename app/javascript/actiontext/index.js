a9e125a9730ccb0ee394be40238753aa99c9e863
Hunt Burdick <hunt@edealerdirect.com>
2019-03-12 00:37:46 -0600

    Bug: Not working in IE 11.

--- app/javascript/actiontext/index.js
+++ app/javascript/actiontext/index.js
@@ -1,7 +1,7 @@
 import * as Trix from "trix"
 import { AttachmentUpload } from "./attachment_upload"
 
-addEventListener("trix-attachment-add", event => {
+addEventListener("trix-attachment-add", function(event) {
   const { attachment, target } = event
 
   if (attachment.file) {
